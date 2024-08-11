f809dc118cd787d5921ee713808ff410a33f8787
petersvc <petersvcosta@gmail.com>
2024-08-11 20:26:59 -0300

    Cria o docker-compose e o README

--- src/main/java/com/iquadras/atalanta/repository/UserRepository.java
+++ src/main/java/com/iquadras/atalanta/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.iquadras.atalanta.repository;
 
-import com.iquadras.atalanta.domain.model.User;
+import com.iquadras.atalanta.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
