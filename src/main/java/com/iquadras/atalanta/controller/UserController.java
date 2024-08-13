5b83f8ae67a3f3c96460c3ee214e1d4ccd2dfea4
LuizManoeldev <dantasluizmanoel@gmail.com>
2024-08-13 11:21:41 -0300

    feat: user login endpoint

--- src/main/java/com/iquadras/atalanta/controller/UserController.java
+++ src/main/java/com/iquadras/atalanta/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.iquadras.atalanta.controller;
 
 import com.iquadras.atalanta.domain.dto.DtoUser;
+import com.iquadras.atalanta.domain.dto.DtoUserLogin;
 import com.iquadras.atalanta.domain.dto.DtoUserPostResponse;
 import com.iquadras.atalanta.service.UserService;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
+
 import jakarta.validation.Valid;
-import org.springframework.beans.BeanUtils;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +23,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Usuário", description = "Controller para gerenciamento/autenticação de usuários")
 public class UserController {
 
+  @Autowired
   private final UserService userService;
 
-  @Autowired
   public UserController(UserService userService) {
     this.userService = userService;
   }
@@ -35,9 +39,22 @@ public class UserController {
         user.getUserId(),
         user.getUserFullName(),
         user.getUserEmail(),
-        user.getUserPhoneNumber()
-    );
+        user.getUserPhoneNumber());
 
     return ResponseEntity.status(HttpStatus.CREATED).body(dtoUserPostResponse);
   }
+
+  @PostMapping("/login")
+  public ResponseEntity<DtoUserPostResponse> userLogin(@RequestBody DtoUserLogin dtoUserLogin) {
+    var user = userService.login(dtoUserLogin);
+
+    var dtoUserPostResponse = new DtoUserPostResponse(
+        user.getUserId(),
+        user.getUserFullName(),
+        user.getUserEmail(),
+        user.getUserPhoneNumber());
+
+    return ResponseEntity.status(HttpStatus.OK).body(dtoUserPostResponse);
+  }
+
 }
